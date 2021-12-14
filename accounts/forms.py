f620b3762f984a82aee6629bf9bfbb5fa5b7abb2
Phuong Dinh Lam Kieu <s1F102000028@iniad.org>
2021-12-14 17:40:07 +0900

    fix forms.py
    Now forms.py works.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,10 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-
+from .models import User
 class CustomUserCreationForm(UserCreationForm):
-    #username=forms.username(help_text="Required. Make it unique")
-    class Meta(UserCreationForm.Meta):
-        help_texts={
-        'username':None
-    }
-        fields = UserCreationForm.Meta.fields + ("email",)
+
+    class Meta:
+        model = User
+        fields = ('email',)
