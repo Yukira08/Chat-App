6ad0f5ee0fe6b63af4c3ebb91a60b1a30043fd34
PHUONG DINH LAM KIEU <s1f102000028@iniad.org>
2021-12-21 15:57:36 +0900

    create room/ enter room save to db

--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
+from .models import Room, message
+from accounts.models import User, Friendship
 import json
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request):
     return render(request, 'chat/index.html', {})
-
+    
+@login_required
 def room(request, room_name):
+    try:
+        a=Room.objects.get(name=room_name)
+    except:
+        a=Room.objects.create(name=room_name)
+        a.save()
+    if request.user not in a.participants.all():
+        a.participants.add(request.user)
+        a.save()
     return render(request, 'chat/room.html', {
         'room_name_json': mark_safe(json.dumps(room_name))
     })
