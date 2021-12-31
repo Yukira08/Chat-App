88ff6e33398147c6bc28f0406044cd395abcb16b
Khin Chan Myae Su <s1f102000062@iniad.org>
2021-12-31 21:15:52 +0900

    Add 'addfriend' icon

--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'chat',
     'authtest',
     'accounts',
+    'django_bootstrap_icons',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
