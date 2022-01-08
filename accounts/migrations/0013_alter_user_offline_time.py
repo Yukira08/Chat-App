47655f19a48aec13d65032a2e6a21643f907bfc9
XUAN ANH TRAN <s1f102010052@iniad.org>
2022-01-08 13:34:23 +0900

    fix migrate bugs

--- accounts/migrations/0013_alter_user_offline_time.py
+++ accounts/migrations/0013_alter_user_offline_time.py
@@ -2,7 +2,7 @@
 
 import datetime
 from django.db import migrations, models
-
+import django.db.models.deletion
 
 class Migration(migrations.Migration):
 
@@ -10,10 +10,27 @@ class Migration(migrations.Migration):
         ('accounts', '0012_alter_user_offline_time_notification'),
     ]
 
+    # operations = [
+    #     migrations.AlterField(
+    #         model_name='user',
+    #         name='offline_time',
+    #         field=models.TimeField(default=datetime.datetime(2022, 1, 8, 10, 28, 54, 346572)),
+    #     ),
+    # ]
     operations = [
         migrations.AlterField(
             model_name='user',
             name='offline_time',
-            field=models.TimeField(default=datetime.datetime(2022, 1, 8, 10, 28, 54, 346572)),
+            field=models.TimeField(default=datetime.datetime(2022, 1, 8, 10, 28, 31, 475874)),
+        ),
+        migrations.CreateModel(
+            name='Notification',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False)),
+                ('noti_type', models.IntegerField()),
+                ('time', models.DateTimeField()),
+                ('receiver', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='receiver', to='accounts.user')),
+                ('sender', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='sender', to='accounts.user')),
+            ],
         ),
     ]
